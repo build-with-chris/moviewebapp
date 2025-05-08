d1c6e9dbc2ef465ca3dd65879453f9e4f0711204
hecking_killa <chris.hermann9397@gmail.com>
2025-05-08 14:52:49 +0200

    finalizing the api

--- api.py
+++ api.py
@@ -1,7 +1,5 @@
-from crypt import methods
-
 from flask import Blueprint, jsonify
-from models import Movie
+from models import Movie, User
 
 api = Blueprint('api', __name__)
 data_manager = None
@@ -12,7 +10,7 @@ def init_api(manager):
 
 @api.route('/users', methods=['GET'])
 def get_users():
-    users = data_manager.get_all_users()
+    users = User.query.all()
     users_dict = [{"user_id": u.user_id, "user_name": u.user_name} for u in users]
     return jsonify(users_dict)
 
@@ -20,8 +18,34 @@ def get_users():
 @api.route('/movies', methods=['GET'])
 def get_movies():
     movies = Movie.query.all()
-    movie_dict = [{"movie_id": m.movie_id, "movie_name": m.movie_name, "movie_year": m.movie_year, "movie_rating": m.movie_rating,
-                   "poster_url": m.poster_url, "imdb_url": m.imdb_url, "user_id": m.user_id,
-                   "reviews": [r.review_text for r in m.reviews]
-                   } for m in movies]
+
+    movie_dict = [{
+        "movie_id": m.movie_id,
+        "movie_name": m.movie_name,
+        "movie_year": m.movie_year,
+        "movie_rating": m.movie_rating,
+        "poster_url": m.poster_url,
+        "imdb_url": m.imdb_url,
+        "user_id": m.user_id,
+        "reviews": [r.review_text for r in m.reviews]
+    } for m in movies]
+
     return jsonify(movie_dict)
+
+
+@api.route('/users/<int:user_id>/movies', methods=['GET'])
+def list_user_favorite_movies(user_id):
+    movies = Movie.query.filter_by(user_id=user_id).all()
+
+    favorites_dict = [{
+        "movie_id": m.movie_id,
+        "movie_name": m.movie_name,
+        "movie_year": m.movie_year,
+        "movie_rating": m.movie_rating,
+        "poster_url": m.poster_url,
+        "imdb_url": m.imdb_url,
+        "user_id": m.user_id,
+        "reviews": [r.review_text for r in m.reviews]
+    } for m in movies]
+
+    return jsonify(favorites_dict)
