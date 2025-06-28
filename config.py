922de766fdcba1dd32ba196c4ef9450d0e03b798
hecking_killa <chris.hermann9397@gmail.com>
2025-06-28 14:13:23 +0200

    prep for deployment on vercel

--- config.py
+++ config.py
@@ -3,10 +3,15 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret')
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        'DATABASE_URL',
-        'sqlite:///' + os.path.join(basedir, 'instance', 'movieweb.db')
-    )
+    # Determine database URI: use external DATABASE_URL if provided,
+    # use tmp SQLite on Vercel (readonly root), otherwise use local file.
+    _db_url = os.getenv('DATABASE_URL')
+    if _db_url:
+        SQLALCHEMY_DATABASE_URI = _db_url
+    elif os.getenv('VERCEL'):
+        SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/movieweb.db'
+    else:
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance', 'movieweb.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     OMDB_API_KEY = os.getenv('OMDB_API_KEY')
     OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
